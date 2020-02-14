84b77b7d2bf8f1ebc93564e3e2f2144957b11607
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-02-14 12:23:42 -0700

    got the ame plate on

--- client/src/Maker.js
+++ client/src/Maker.js
@@ -2,6 +2,17 @@ import React from 'react';
 import axios from 'axios';
 import Chart from "./components/Chart";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+function CustomTooltip({ payload, label, active }) {
+    if (active) {
+        return (
+
+            <div className="custom-tooltip">
+                <p className="name">{`${label} : ${payload[0].value}`}</p>
+                <p className="intro">{`${payload[0].payload.country}`}</p>
+            </div>
+        );
+    }
+}
 class WomenMaker extends React.Component {
     constructor() {
         super();
@@ -22,13 +33,12 @@ class WomenMaker extends React.Component {
     render() {
         return (
             <div className="runners" >
-                runners
                 <LineChart width={1000} height={500} data={this.state.runner}>
                     <Line type="monotone" dataKey="searches" stroke="#8884d8" />
                     <CartesianGrid stroke="#ccc" />
                     <XAxis dataKey="name" />
                     <YAxis />
-                    <Tooltip />
+                    <Tooltip content={CustomTooltip} />
                 </LineChart>
             </div>
         );
